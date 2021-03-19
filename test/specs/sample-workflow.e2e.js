ccebaa9517a65dc3f277315326f1d8a621bf6576
maxwellrk <maxkatz23@gmail.com>
2021-03-19 17:23:01 -0400

    Created end to end spec file

---
+++ test/specs/sample-workflow.e2e.js
@@ -0,0 +1,10 @@
+const HomePage = require('../pageobjects/home.page');
+const CartPage = require('../pageobjects/cart.page');
+const CheckoutPage = require('../pageobjects/checkout.page');
+const InventoryPage = require('../pageobjects/inventory.page');
+
+describe('Primary Workflow', () => {
+    it('Cart Page - Adds correct items in cart', () => {});
+    it('Checkout Page - Adds correct items in checkout', () => {});
+    it('Checkout Page - Calculates the items total accuratley', () => {});
+});
