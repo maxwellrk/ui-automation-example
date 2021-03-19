e507e64e27450ec7500ceb7812d9bef8b2c8a844
maxwellrk <maxkatz23@gmail.com>
2021-03-19 16:54:16 -0400

    Added removal from cart methods

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -8,7 +8,32 @@ class CartPage extends Page {
     /**
      * getter selectors for cart page
      */
+    //continue shopping button
+
+    //checkout button
+
     //Methods
+    /**
+     * Finds and returns an element object based on it's child div.inventory_item_name
+     * @param {string} itemName Text value of div.inventory_item_name to find
+     * will likely abstract this function into a new super class
+     */
+    findItemByItemName(itemName) {
+        return $(`a=${itemName}`)
+            .parentElement()
+            .parentElement()
+            .parentElement();
+    }
+
+    /**
+     * Removes an item from the cart from the cart page by the item's name
+     * @param {string} itemName Text value of div.inventory_item_name to use
+     */
+    removeItemFromCart(itemName) {
+        this.findItemByItemName(itemName)
+            .$('.btn_secondary.cart_button')
+            .click();
+    }
 }
 
 module.exports = new CartPage();
