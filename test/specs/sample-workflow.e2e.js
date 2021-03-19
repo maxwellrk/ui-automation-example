e99c2d026b5d5d991e6380043d38e0b0e2fe3e35
maxwellrk <maxkatz23@gmail.com>
2021-03-19 17:41:02 -0400

    Added Cart Page - Adds correct items in cart it block

--- test/specs/sample-workflow.e2e.js
+++ test/specs/sample-workflow.e2e.js
@@ -4,7 +4,20 @@ const CheckoutPage = require('../pageobjects/checkout.page');
 const InventoryPage = require('../pageobjects/inventory.page');
 
 describe('Primary Workflow', () => {
-    it('Cart Page - Adds correct items in cart', () => {});
+    it('Cart Page - Adds correct items in cart', () => {
+        HomePage.open();
+        HomePage.login('standard_user', 'secret_sauce');
+        InventoryPage.selectSortByDropdownByText('Price (low to high)');
+        InventoryPage.addItemToCart('Sauce Labs Backpack');
+        InventoryPage.addItemToCart('Sauce Labs Bike Light');
+        InventoryPage.addItemToCart('Test.allTheThings() T-Shirt (Red)');
+        InventoryPage.shoppingCartLink.click();
+        expect(CartPage.findItemByItemName('Sauce Labs Backpack')).toExist();
+        expect(CartPage.findItemByItemName('Sauce Labs Bike Light')).toExist();
+        expect(
+            CartPage.findItemByItemName('Test.allTheThings() T-Shirt (Red)')
+        ).toExist();
+    });
     it('Checkout Page - Adds correct items in checkout', () => {});
     it('Checkout Page - Calculates the items total accuratley', () => {});
 });
