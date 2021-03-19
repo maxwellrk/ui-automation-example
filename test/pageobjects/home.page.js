95103cdb94758bbd3994cd5e31b6bfc619320bff
maxwellrk <maxkatz23@gmail.com>
2021-03-19 14:08:10 -0400

    Created home page object model, which extends the page object

---
+++ test/pageobjects/home.page.js
@@ -0,0 +1,15 @@
+const Page = require('./page');
+
+/**
+ * Home page selectors and methods
+ */
+class HomePage extends Page {
+    /**
+     * getter selectors for home page
+     */
+    /**
+     * methods for encapsulation
+     */
+}
+
+module.exports = new HomePage();
