7cfbe9c17e956dc363c07d8c58d5c9bfdc13d168
maxwellrk <maxkatz23@gmail.com>
2021-03-19 16:31:16 -0400

    Created checkout page object model

---
+++ test/pageobjects/checkout.page.js
@@ -0,0 +1,14 @@
+const Page = require('./page');
+
+/**
+ * Checkout page selectors and methods
+ */
+
+class CheckoutPage extends Page {
+    /**
+     * getter selectors for checkout page
+     */
+    //Methods
+}
+
+module.exports = new CheckoutPage();
