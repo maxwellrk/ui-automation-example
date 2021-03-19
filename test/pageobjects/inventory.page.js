96c4fb4ab6c81b7c966c104f683c9094023c2a6b
maxwellrk <maxkatz23@gmail.com>
2021-03-19 16:30:50 -0400

    Removed arrayOfInventoryItems getter method, added findItemByItemName and addItemToCart methods

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -11,22 +11,38 @@ class InventoryPage extends Page {
         return $('select.product_sort_container');
     }
 
-    get arrayOfInventoryItems() {
-        return $('div.inventory_item');
-    }
-
     get shoppingCartLink() {
         return $('a.shopping_cart_link');
     }
 
     //Methods
-
     /**
      * Selects sortby dropdown option by text
      * @param {string} selectedOption dropdown option to select by visible text
      */
     selectSortByDropdownByText(selectedOption) {
         this.sortByDropdown.selectByVisibleText(selectedOption);
+    }
+
+    /**
+     * Finds and returns an element object based on it's child div.inventory_item_name
+     * @param {string} itemName Text value of div.inventory_item_name to find
+     */
+    findItemByItemName(itemName) {
+        return $(`a=${itemName}`)
+            .parentElement()
+            .parentElement()
+            .parentElement();
+    }
+
+    /**
+     * Adds an item to the cart from the inventory page by the item's name
+     * @param {string} itemName Text value of div.inventory_item_name to use
+     */
+    addItemToCart(itemName) {
+        this.findItemByItemName(itemName)
+            .$('.btn_primary.btn_inventory')
+            .click();
     }
 }
 
