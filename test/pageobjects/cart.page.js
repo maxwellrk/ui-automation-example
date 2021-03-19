a139ef76f862e54ebe592c4d146fa5f56d2a7b12
maxwellrk <maxkatz23@gmail.com>
2021-03-19 16:39:00 -0400

    Added cart page object model

---
+++ test/pageobjects/cart.page.js
@@ -0,0 +1,14 @@
+const Page = require('./page');
+
+/**
+ * Cart page selectors and methods
+ */
+
+class CartPage extends Page {
+    /**
+     * getter selectors for cart page
+     */
+    //Methods
+}
+
+module.exports = new CartPage();
