c5bf3398d6230f3a293b13f2380c6eebcde1921e
maxwellrk <maxkatz23@gmail.com>
2021-03-20 13:51:13 -0400

    Added getters and methods to checkout step 2 page object model

--- test/pageobjects/checkout-step2.page.js
+++ test/pageobjects/checkout-step2.page.js
@@ -8,7 +8,26 @@ class CheckoutStep2Page extends Page {
     /**
      * getter selectors for checkout page
      */
+
+    //total price
+    get totalPrice() {
+        return $('.summary_total_label');
+    }
+    //finish button
+    get finishButton() {
+        return $('a.btn_action.cart_button');
+    }
+
     //Methods
+    /**
+     * Finds and returns an element object based on it's child div.inventory_item_name
+     * @param {string} itemName Text value of div.inventory_item_name to find
+     * will likely abstract this function into a new super class
+
+     */
+    findItemByItemName(itemName) {
+        return $(`div=${itemName}`);
+    }
 }
 
 module.exports = new CheckoutStep2Page();
